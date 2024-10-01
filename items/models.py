75adaab431abf9bd018f73c45aaf9df5276fa7dc
Adm <hbadam903@gmail.com>
2024-10-01 05:57:44 -0700

    Update models.py

--- items/models.py
+++ items/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Vehicle(models.Model):
+    vehicle_type = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.vehicle_type
+
 # Create your models here.
