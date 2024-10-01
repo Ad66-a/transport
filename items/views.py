68b78407f5927db569e0ac0b145d96f7aa6b1c89
Adm <hbadam903@gmail.com>
2024-10-01 06:07:19 -0700

    Update views.py

--- items/views.py
+++ items/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Vehicle
+from .forms import VehicleForm
+
+# READ: List all vehicles
+def vehicle_list(request):
+    vehicles = Vehicle.objects.all()
+    return render(request, 'vehicles/vehicle_list.html', {'vehicles': vehicles})
+
+# CREATE: Create a new vehicle
+def vehicle_create(request):
+    if request.method == 'POST':
+        form = VehicleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('vehicle_list')
+    else:
+        form = VehicleForm()
+    return render(request, 'vehicles/vehicle_form.html', {'form': form})
+
+# UPDATE: Update an existing vehicle
+def vehicle_update(request, id):
+    vehicle = Vehicle.objects.get(id=id)
+    if request.method == 'POST':
+        form = VehicleForm(request.POST, instance=vehicle)
+        if form.is_valid():
+            form.save()
+            return redirect('vehicle_list')
+    else:
+        form = VehicleForm(instance=vehicle)
+    return render(request, 'vehicles/vehicle_form.html', {'form': form})
+
+# DELETE: Delete a vehicle
+def vehicle_delete(request, id):
+    vehicle = Vehicle.objects.get(id=id)
+    if request.method == 'POST':
+        vehicle.delete()
+        return redirect('vehicle_list')
+    return render(request, 'vehicles/vehicle_confirm_delete.html', {'vehicle': vehicle})
 
 # Create your views here.
